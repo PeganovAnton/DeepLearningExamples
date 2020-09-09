eff872fcde3a5af745c9686d039bc0c03ea11d5c
PeganovAnton <peganoff2@mail.ru>
2020-09-09 09:08:46 -0700

    Fix debug command

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/debug_commands/vary_num_mem_tokens.sh
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/debug_commands/vary_num_mem_tokens.sh
@@ -1,6 +1,5 @@
 for nmt in 0 1 2 3 4 6 9 13 19 28 42; do
-    bash run_wt103_small_mem_tokens_simple_relative.sh train 8 \
-        --config dgx1_8gpu_fp16 \
+    bash run_wt103_small_mem_tokens_simple_relative.sh train 2 \
         --num_mem_tokens ${nmt} \
         --mem_len "$(expr 192 + ${nmt})"
 done
