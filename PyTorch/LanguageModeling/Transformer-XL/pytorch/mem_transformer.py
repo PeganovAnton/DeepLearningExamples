ef55e4360d214dcd640db2baae49bc78a0191b9f
PeganovAnton <peganoff2@mail.ru>
2020-09-14 11:32:02 +0300

    backup

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/mem_transformer.py
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/mem_transformer.py
@@ -719,7 +719,7 @@ class MemTransformerLM(nn.Module):
             dim=1
         ).bool()
         dec_attn_mask = torch.cat((prepended_query_mask, dec_attn_mask), dim=0)
-        torch.set_printoptions(linewidth=1000)
+        torch.set_printoptions(linewidth=1000, threshold=10000)
         print("(MemTransformerLM._forward)dec_attn_mask.shape:", dec_attn_mask.shape)
         print("(MemTransformerLM._forward)dec_attn_mask:", dec_attn_mask.byte())
         hids = []
