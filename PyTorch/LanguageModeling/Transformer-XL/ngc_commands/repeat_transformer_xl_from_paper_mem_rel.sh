7ba429d92138f330f45049fcebd1f1e5e0eeff24
PeganovAnton <peganoff2@mail.ru>
2020-09-28 10:35:31 -0700

    Add mem rel configs from paper

---
+++ PyTorch/LanguageModeling/Transformer-XL/ngc_commands/repeat_transformer_xl_from_paper_mem_rel.sh
@@ -0,0 +1,18 @@
+cd DeepLearningExamples/PyTorch/LanguageModeling/Transformer-XL \
+  && pip install -r requirements.txt \
+  && pip install --no-cache-dir git+https://github.com/NVIDIA/dllogger.git#egg=dllogger \
+  && mkdir -p data \
+  && cd data \
+  && wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip \
+  && unzip -q wikitext-103-v1.zip \
+  && cd wikitext-103 \
+  && mv wiki.train.tokens train.txt \
+  && mv wiki.valid.tokens valid.txt \
+  && mv wiki.test.tokens test.txt \
+  && cd ../.. \
+  && cd pytorch \
+  && for i in {1..5}; do
+       bash run_wt103_orig_base_mem_rel.sh train 8 --config dgx1_8gpu_fp32 \
+         --work_dir /result/repeat${i} --seed ${i}
+     done 
+
